c9691b3d4624243749d0c1bb185a1d41a570af0d
Kieran O'Brien <developer@obrien.network>
2019-09-09 23:05:45 +1000

    Added dagger data module with @provides for OkHttpClient and and IsThereAnyDeal retrofit service.

---
+++ app/src/main/java/network/obrien/isthereanydeal/di/DataModule.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018-present Kieran O'Brien
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package network.obrien.isthereanydeal.di
+
+import dagger.Lazy
+import dagger.Module
+import dagger.Provides
+import network.obrien.isthereanydeal.data.api.IsThereAnyDealService
+import network.obrien.isthereanydeal.util.BigDecimalAdapter
+import network.obrien.isthereanydeal.util.httpClient
+import network.obrien.isthereanydeal.util.moshiConverterFactory
+import network.obrien.isthereanydeal.util.service
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+@Module
+class DataModule {
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = httpClient()
+
+    @Provides
+    @IsThereAnyDealPublicApi
+    fun provideIsThereAnyDealService(
+        httpClient: Lazy<OkHttpClient>
+    ): IsThereAnyDealService = Retrofit.Builder().service(
+        IsThereAnyDealService.ENDPOINT,
+        httpClient.get(),
+        moshiConverterFactory(BigDecimalAdapter)
+    )
+}
