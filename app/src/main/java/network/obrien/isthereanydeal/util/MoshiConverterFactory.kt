a84f5860c87768b4bd62579ff4cf0406ff1e1459
Kieran O'Brien <developer@obrien.network>
2019-09-22 01:29:50 +1000

    Code cleanup and re-org for more descriptive naming and relevant grouping in util package.

--- app/src/main/java/network/obrien/isthereanydeal/util/MoshiConverterFactory.kt
+++ app/src/main/java/network/obrien/isthereanydeal/util/MoshiConverterFactory.kt
@@ -17,21 +17,10 @@
 
 package network.obrien.isthereanydeal.util
 
-import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.ToJson
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.math.BigDecimal
 
-fun moshiConverterFactory(vararg adapters: Any): MoshiConverterFactory =
-    MoshiConverterFactory.create(
-        Moshi.Builder().apply { adapters.forEach { adapter -> add(adapter) } }.build()
-    )
-
-object BigDecimalAdapter {
-    @FromJson
-    fun fromJson(json: String): BigDecimal = BigDecimal(json)
-
-    @ToJson
-    fun toJson(bigDecimal: BigDecimal): String = bigDecimal.toString()
-}
+fun moshiConverterFactory(vararg adapters: Any) = Moshi.Builder()
+    .apply { adapters.forEach { adapter -> add(adapter) } }
+    .build()
+    .let { moshi -> MoshiConverterFactory.create(moshi) }
